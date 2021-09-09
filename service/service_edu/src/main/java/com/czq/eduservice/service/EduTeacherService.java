b02caa759513049c6d12c24db75613ca86b57d1b
chuzhaoqiang <chuzhaoqiang@csii.com.cn>
2021-09-09 09:59:12 +0800

    Initial commit

---
+++ service/service_edu/src/main/java/com/czq/eduservice/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.czq.eduservice.service;
+
+import com.czq.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author testjava
+ * @since 2021-09-07
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/czq/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.czq.eduservice.service.impl;
+
+import com.czq.eduservice.entity.EduTeacher;
+import com.czq.eduservice.mapper.EduTeacherMapper;
+import com.czq.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2021-09-07
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
